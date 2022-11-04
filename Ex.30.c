650532ae5bf7156b26b89a630e868bc0140f1b73
victorgavasenema <116038690+victorgavasenema@users.noreply.github.com>
2022-11-03 21:32:57 -0300

    Update Ex.30.c

--- Ex.30.c
+++ Ex.30.c
@@ -8,27 +8,27 @@
     printf ("digite 3 numeros:\n");
     scanf ("%f%f%f", &x, &y, &z);
 
-    if (x > y && y > z){
+    if (x < y && y < z){
     printf ("A sequencia em ordem crescente eh: %.2f, %.2f, %.2f\n", x, y, z);
     }
     
-    if (x > z && z > y){
+    if (x < z && z < y){
     printf ("A sequencia em ordem crescente eh: %.2f, %.2f, %.2f\n", x, z, y);
     }
 
-    if (y > x && x > z){
+    if (y < x && x < z){
     printf ("A sequencia em ordem crescente eh: %.2f, %.2f, %.2f\n", y, x, z);
     }
 
-    if (y > z && z > x){
+    if (y < z && z < x){
     printf ("A sequencia em ordem crescente eh: %.2f, %.2f, %.2f\n", y, z, x);
     }
 
-    if (z > x && x > y){
+    if (z < x && x < y){
     printf ("A sequencia em ordem crescente eh: %.2f, %.2f, %.2f\n", z, x, y);
     }
 
-    if (z > y && y > x){
+    if (z < y && y < x){
     printf ("A sequencia em ordem crescente eh: %.2f, %.2f, %.2f\n", z, y, x);
     }
 
